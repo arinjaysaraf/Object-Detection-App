947842b77f5918a0b183d312821d52f60f9fd515
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-06-24 00:13:01 +0530

    Updates

---
+++ precache-manifest.e8561be200d13e5a4897e55843672ffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e267c1321efb09bcf41043732ba18a82",
+    "url": "/arinjaysaraf/Object-Detection-App/index.html"
+  },
+  {
+    "revision": "abc89ce0df873a33faf4",
+    "url": "/arinjaysaraf/Object-Detection-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a9c64c6f2e24fc63b79",
+    "url": "/arinjaysaraf/Object-Detection-App/static/js/2.711ec297.chunk.js"
+  },
+  {
+    "revision": "e014ead73f636818c4254719a1ba0cb8",
+    "url": "/arinjaysaraf/Object-Detection-App/static/js/2.711ec297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc89ce0df873a33faf4",
+    "url": "/arinjaysaraf/Object-Detection-App/static/js/main.1e80e5ee.chunk.js"
+  },
+  {
+    "revision": "bb89827b21e637487926",
+    "url": "/arinjaysaraf/Object-Detection-App/static/js/runtime-main.22cc9a2e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/arinjaysaraf/Object-Detection-App/precache-manifest.e8561be200d13e5a4897e55843672ffb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arinjaysaraf/Object-Detection-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
